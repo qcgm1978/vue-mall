19cce85a4842f8c026ca795def56181f8d2bd7d4
qcgm1978 <zhanghl@strongunion.com.cn>
2018-08-01 10:47:15 +0800

    ditto

--- client/build/webpack.dev.conf.js
+++ client/build/webpack.dev.conf.js
@@ -76,7 +76,7 @@ module.exports = new Promise((resolve, reject) => {
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
           messages: [`Your application is running here: http://${config.dev.host}:${port}
-          Backstage is running here: http://${config.dev.host}:${port}/admin/#/login`],
+          Backstage is running here: http://${config.dev.host}:${port}/admin.html`],
         },
         onErrors: config.dev.notifyOnErrors
           ? utils.createNotifierCallback()
